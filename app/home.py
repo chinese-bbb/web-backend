6e7270e2024c8f5fdb30a7118c5ff25bb074b162
e-cloud <saintscott119@gmail.com>
2019-08-03 15:40:37 +0800

    chore: experimental for versioning eb deploy bundle
    we need to remove .ebignore for ebcli to use git for bundle archiving
    see https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-configuration.html#eb-cli3-ebignore
    
    .ebignore is required only when developer need to deploy code with `eb deploy --staged`
    so we ignore it in gitignore

--- app/home.py
+++ app/home.py
@@ -3,7 +3,10 @@ from flask import render_template
 
 bp = Blueprint('home', __name__, template_folder='../templates')
 
+with open('version.txt') as f:
+    git_hash = f.readline()
+
 
 @bp.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', hash=git_hash)
