531092cfd2c62b6fc6d95fe703e3ae8ab28d5b68
e-cloud <saintscott119@gmail.com>
2019-08-07 02:04:20 +0800

    feat: support international number registration

--- app/resources/auth/routes.py
+++ app/resources/auth/routes.py
@@ -4,6 +4,7 @@ import logging
 import random
 
 import flask
+import phonenumbers
 from flask import after_this_request
 from flask import flash
 from flask import request
@@ -13,13 +14,12 @@ from flask_login import login_user
 from flask_login import logout_user
 from flask_restplus import fields
 from flask_restplus import Namespace
+from flask_restplus import reqparse
 from flask_restplus import Resource
 
-from app.extensions import api
 from app.extensions import db
 from app.resources.users.models import User
 from app.services.tencent.id_ocr import tencent_ocr
-from app.services.tencent.send_sms import send_message
 
 log = logging.getLogger(__name__)
 
@@ -30,55 +30,71 @@ messageDict = {}
 
 
 @ns.route('/phone_exist/<string:phone_num>')
-@api.doc(params={'phone_num': 'A phone number'})
+@ns.doc(params={'phone_num': 'A phone number'})
 class PhoneExist(Resource):
     """Check if phone number is registered."""
 
-    @api.doc(responses={200: 'Success', 400: 'Validation Error'})
+    @ns.doc(responses={200: 'Success', 404: 'Validation Error'})
     def get(self, phone_num):
         user = User.query.filter_by(username=phone_num).first()
         log.debug(user)
         if user is None:
-            return {phone_num: 'Not Registerred phone num'}, 400
+            try:
+                result = phonenumbers.parse(phone_num, 'CN')
+                if result.country_code == 86:
+                    phone_num = result.national_number
+                user = User.query.filter_by(username=phone_num).first()
+            except Exception:
+                return {'error': 'Invalid phone num or password'}, 404
+
+            if user is None:
+                return {phone_num: 'Not Registerred phone num'}, 404
         return {'state': 'Success'}, 200
 
 
-sms_parser = api.parser()
-sms_parser.add_argument(
-    'v_code', type=str, required=True, help='verification code', location='json'
+reuqest_sms_parser = reqparse.RequestParser()
+reuqest_sms_parser.add_argument(
+    'national_number', type=str, required=True, help='national_number', location='json'
+)
+reuqest_sms_parser.add_argument(
+    'country_code', type=str, required=True, help='country_code', location='json'
 )
 
 
-@ns.route('/sms/<string:phone_num>')
-@api.doc(params={'phone_num': 'A phone number'})
-@api.doc(
+@ns.route('/sms/request')
+@ns.doc(params={'national_number': 'A phone number', 'country_code': 'country_code'})
+@ns.doc(
     responses={200: 'Success', 400: 'Validation Error', 401: 'Passcode is not correct'}
 )
-class SMS(Resource):
-    def get(self, phone_num):
+class RequestSMS(Resource):
+    @ns.doc(parser=reuqest_sms_parser)
+    def post(self):
         """Get verification code for a phone number."""
         rand_num = random.randint(1000, 9999)
-        msg = send_message(phone_num, rand_num)
-        sid = msg['sid']
+        reuqest_sms_parser.parse_args()
+        # msg = send_message(args['national_number'], args['country_code'], rand_num)
+        sid = "msg['sid']"
         messageDict[sid] = str(rand_num)
         log.debug(rand_num)
         return {'state': 'Success'}, 200, {'Set-Cookie': 'sid=' + sid}
 
-    def _check_cookie_sid(self):
-        if request.cookies.get('sid') is None:
-            api.abort(403, "The cookie doesn't come with sid entry")
 
-        @after_this_request
-        def set_register_cookie(response):
-            response.set_cookie('phone_auth', 'yes', max_age=64800)
-            return response
+validate_sms_parser = reqparse.RequestParser()
+validate_sms_parser.add_argument(
+    'v_code', type=str, required=True, help='verification code', location='json'
+)
 
-        return request.cookies.get('sid')
 
-    @api.doc(parser=sms_parser)
-    def post(self, phone_num):
+@ns.route('/sms/validate')
+@ns.doc(params={'v_code': 'sms code'})
+@ns.doc(
+    responses={200: 'Success', 400: 'Validation Error', 401: 'Passcode is not correct'}
+)
+class ValidateSMS(Resource):
+    @ns.doc(parser=validate_sms_parser)
+    def post(self):
         """Verify the verification number."""
-        args = sms_parser.parse_args()
+        args = validate_sms_parser.parse_args()
         v_code = args['v_code']
 
         sid = self._check_cookie_sid()
@@ -87,8 +103,19 @@ class SMS(Resource):
             return {'error': 'verification code is not correct'}, 401
         return {'state': 'Success'}, 200
 
+    def _check_cookie_sid(self):
+        if request.cookies.get('sid') is None:
+            ns.abort(403, "The cookie doesn't come with sid entry")
 
-login_parser = api.parser()
+        @after_this_request
+        def set_register_cookie(response):
+            response.set_cookie('phone_auth', 'yes', max_age=64800)
+            return response
+
+        return request.cookies.get('sid')
+
+
+login_parser = ns.parser()
 login_parser.add_argument(
     'phone_num', type=str, required=True, help='Phone Number', location='json'
 )
@@ -98,7 +125,7 @@ login_parser.add_argument(
 
 
 @ns.route('/login')
-@api.doc(
+@ns.doc(
     responses={
         200: 'Success',
         401: 'Invalid password',
@@ -107,7 +134,7 @@ login_parser.add_argument(
     }
 )
 class Login(Resource):
-    @api.doc(parser=login_parser)
+    @ns.doc(parser=login_parser)
     def post(self):
         """Log in."""
         args = login_parser.parse_args()
@@ -117,7 +144,16 @@ class Login(Resource):
         user = User.query.filter_by(username=username).first()
         log.debug(user)
         if user is None:
-            return {'error': 'User cannot be found'}, 404
+            try:
+                result = phonenumbers.parse(username, 'CN')
+                if result.country_code == 86:
+                    username = result.national_number
+                user = User.query.filter_by(username=username).first()
+            except Exception:
+                return {'error': 'Invalid phone num or password'}, 404
+
+            if user is None:
+                return {'error': 'Invalid phone num or password'}, 404
 
         if not user.check_password(password):
             log.debug('Invalid username or password')
@@ -128,7 +164,7 @@ class Login(Resource):
 
 
 @ns.route('/logout')
-@api.doc(responses={200: 'Success'})
+@ns.doc(responses={200: 'Success'})
 class Logout(Resource):
     @login_required
     def post(self):
@@ -143,7 +179,7 @@ class EnumSexType(enum.Enum):
     other = 'others'
 
 
-register_fields = api.model(
+register_fields = ns.model(
     'RegisterModel',
     {
         'sex': fields.String(
@@ -159,18 +195,18 @@ register_fields = api.model(
 
 
 @ns.route('/register')
-@api.doc(responses={200: 'Register Success', 400: 'Validation Error'})
+@ns.doc(responses={200: 'Register Success', 400: 'Validation Error'})
 class Register(Resource):
     def _check_cookie_phone_allow(self):
         if request.cookies.get('phone_auth') is None:
-            api.abort(403, "The cookie doesn't come with phone_allow entry")
+            ns.abort(403, "The cookie doesn't come with phone_allow entry")
 
-    @api.expect(register_fields)
+    @ns.expect(register_fields)
     def post(self):
         """Register a user."""
 
         self._check_cookie_phone_allow()
-        data = api.payload
+        data = ns.payload
         username = data['phone_num']
         sex = data['sex']
         password = data['password']
@@ -194,7 +230,7 @@ class Register(Resource):
         return flask.jsonify('OK')
 
 
-changepwd_parser = api.parser()
+changepwd_parser = ns.parser()
 changepwd_parser.add_argument(
     'phone_num', type=str, required=True, help='Phone Number', location='json'
 )
@@ -207,9 +243,9 @@ changepwd_parser.add_argument(
 
 
 @ns.route('/changepw')
-@api.doc(responses={200: 'Success', 400: 'Validation Error'})
+@ns.doc(responses={200: 'Success', 400: 'Validation Error'})
 class ChangePassword(Resource):
-    @api.doc(parser=changepwd_parser)
+    @ns.doc(parser=changepwd_parser)
     def post(self):
         """Change Password."""
 
@@ -230,7 +266,7 @@ class ChangePassword(Resource):
         return flask.jsonify('OK')
 
 
-resetpwd_parser = api.parser()
+resetpwd_parser = ns.parser()
 resetpwd_parser.add_argument(
     'phone_num', type=str, required=True, help='Phone Number', location='json'
 )
@@ -240,9 +276,9 @@ resetpwd_parser.add_argument(
 
 
 @ns.route('/resetpw')
-@api.doc(responses={200: 'Success', 400: 'Validation Error'})
+@ns.doc(responses={200: 'Success', 400: 'Validation Error'})
 class ResetPassword(Resource):
-    @api.doc(parser=resetpwd_parser)
+    @ns.doc(parser=resetpwd_parser)
     def post(self):
         """Reset Password."""
 
@@ -262,17 +298,17 @@ class ResetPassword(Resource):
         return flask.jsonify('OK')
 
 
-identify_parser = api.parser()
+identify_parser = ns.parser()
 identify_parser.add_argument(
     'id_path', type=str, required=True, help='upload path', location='json'
 )
 
 
 @ns.route('/identify')
-@api.doc(responses={200: 'Success', 400: 'Validation Error'})
+@ns.doc(responses={200: 'Success', 400: 'Validation Error'})
 class IdentifyIDCard(Resource):
     @login_required
-    @api.doc(parser=identify_parser)
+    @ns.doc(parser=identify_parser)
     def post(self):
         """Identify user ID card."""
         user_id = session['user_id']
--- app/services/tencent/send_sms.py
+++ app/services/tencent/send_sms.py
@@ -6,7 +6,7 @@ import os
 log = logging.getLogger(__name__)
 
 
-def send_message(phone, random_num):
+def send_message(mobile_number, country_code, random_num):
 
     appid = 1400205630  # SDK AppID 是1400开头
 
@@ -14,7 +14,7 @@ def send_message(phone, random_num):
     appkey = os.environ['TENCENT_APPKEY']
 
     # 需要发送短信的手机号码
-    phone_numbers = [phone]
+    mobile_numbers = [mobile_number]
 
     # 短信模板 ID，需要在短信应用中申请
     template_id = 324017  # NOTE: 这里的模板 ID`7839`只是一个示例，真实的模板 ID 需要在短信控制台中申请
@@ -33,7 +33,13 @@ def send_message(phone, random_num):
     ]  # 当模板没有参数时，`params = []`，数组具体的元素个数和模板中变量个数必须一致，例如示例中 templateId:5678 对应一个变量，参数数组中元素个数也必须是一个
     try:
         result = ssender.send_with_param(
-            86, phone_numbers[0], template_id, params, sign=sms_sign, extend='', ext=''
+            country_code,
+            mobile_numbers[0],
+            template_id,
+            params,
+            sign=sms_sign,
+            extend='',
+            ext='',
         )  # 签名参数未提供或者为空时，会使用默认签名发送短信
     except HTTPError as e:
         log.debug(e)
