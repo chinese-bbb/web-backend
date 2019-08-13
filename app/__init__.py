da7b1ec4e21172b287d46ecc73c85fc1646f6f6b
e-cloud <saintscott119@gmail.com>
2019-08-13 12:50:59 +0800

    feat: support logging global exceptions and each request

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
+import logging
+import traceback
+from http import HTTPStatus
+
 from flask import Flask
+from flask import request
 
 from app.extensions import api
 
@@ -36,4 +41,36 @@ def create_app(app_name, config_or_path=None, **kwargs):
     api.register_blueprint(api_blueprint, url_prefix='/api')
     app.register_blueprint(home_blueprint, url_prefix='/')
 
+    define_global_interception(app)
+
     return app
+
+
+def define_global_interception(app):
+    log = logging.getLogger(__name__)
+
+    @app.errorhandler(Exception)
+    def handle_global_exception():
+        tb = traceback.format_exc()
+        log.error(
+            '%s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+            request.remote_addr,
+            request.method,
+            request.scheme,
+            request.full_path,
+            tb,
+        )
+
+        return 'oohs! something goes wrong', HTTPStatus.INTERNAL_SERVER_ERROR
+
+    @app.after_request
+    def after_request(response):
+        log.info(
+            '%s %s %s %s %s',
+            request.remote_addr,
+            request.method,
+            request.scheme,
+            request.full_path,
+            response.status,
+        )
+        return response
