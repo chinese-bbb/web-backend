5e2030d429b990ca20eab7d1bd4bff17de99d2a8
Liang Tang <liang.xjtu@gmail.com>
2019-08-27 16:48:08 -0700

    fix delete script issues

--- tools/delete.py
+++ tools/delete.py
@@ -5,16 +5,14 @@ import sqlite3
 sqlite_file = '/home/wsgi/app.db'  # name of the sqlite database file
 table1 = 'user'
 field1 = 'username'
-data1 = '13750046503'
+data1 = '+13343326180'
 
 # Connecting to the database file
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
-# c.execute("SELECT * FROM user WHERE username = '13750046503'")
-
 c.execute(
-    'SELECT * FROM {table1} WHERE {field1} = {data1} '.format(
+    "SELECT * FROM {table1} WHERE {field1} = \'{data1}\' ".format(
         table1=table1, field1=field1, data1=data1
     )
 )
@@ -22,14 +20,15 @@ c.execute(
 all_rows = c.fetchall()
 print('1):', all_rows)
 
-# c.execute("DELETE FROM user WHERE username = '13750046503'")
-# c.execute("DELETE FROM {user} WHERE {field1} = {data1}"\
-#           .format(table1=table1, field1=field1, data1=data1))
+c.execute(
+    "DELETE FROM {table1} WHERE {field1} = \'{data1}\' ".format(
+        table1=table1, field1=field1, data1=data1
+    )
+)
 conn.commit()
 
-# c.execute("SELECT * FROM user WHERE username = '13750046503'")
 c.execute(
-    'SELECT * FROM {user} WHERE {field1} = {data1}'.format(
+    "SELECT * FROM {table1} WHERE {field1} = \'{data1}\' ".format(
         table1=table1, field1=field1, data1=data1
     )
 )
