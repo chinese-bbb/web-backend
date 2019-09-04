04232a1fd4829bb27a14a98c11e3f8cee8d30030
Liang Tang <liang.xjtu@gmail.com>
2019-09-04 10:02:30 -0700

    fix: to allow zsh work for precommit

--- tools/freeze-requirements.py
+++ tools/freeze-requirements.py
@@ -13,6 +13,17 @@ if os.name == 'nt':
     )
     process.stdin.write('./env/Scripts/activate\n')
     process.stdin.write('$PSDefaultParameterValues["Out-File:Encoding"] = "ASCII"\n')
+elif 'zsh' in os.environ.get('SHELL'):
+    process = Popen(
+        '/bin/zsh',
+        shell=False,
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+        universal_newlines=True,
+    )
+    print('access zsh env')
+    process.stdin.write('source ./env/bin/activate\n')
 else:
     process = Popen(
         '/bin/sh',
@@ -24,8 +35,9 @@ else:
     )
     process.stdin.write('source ./env/bin/activate\n')
 
-
 process.stdin.write('pip freeze > requirements.txt\n')
+process.stdin.flush()
 process.stdin.write('git add requirements.txt\n')
 process.stdin.write('deactivate')
+process.stdin.flush()
 process.stdin.close()
